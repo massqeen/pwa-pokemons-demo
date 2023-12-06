4dc54c563cf01d7c5a351b3bf552124f91ec5e60
Arthur Pliushchevskyi <ap@hilfy.net>
2023-12-06 12:29:42 +0100

    Updated PWA config, added draft offline page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const withPWA = require('next-pwa')({
     dest: 'public',
-    disable: process.env.NODE_ENV === 'development'
+    disable: process.env.NODE_ENV === 'development',
+    cacheStartUrl: true,
+    cacheOnFrontendNav: true,
+    reloadOnOnline: true
 })
 
 module.exports = withPWA({
---
+++ pages/_offline.tsx
@@ -0,0 +1,8 @@
+export default function Offline() {
+    return (
+        <>
+            <h1>This is offline fallback page</h1>
+            <h2>When offline, any page route will fallback to this page</h2>
+        </>
+    );
+}
