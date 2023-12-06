31b3b1a2bb6de878a30b2d3166e3fea73f362d88
Arthur Pliushchevskyi <ap@hilfy.net>
2023-12-06 11:21:20 +0100

    Added app Head info such as title, meta and icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
 import '@/styles/globals.css'
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (<>
+    <Head>
+      <title>Pokemon</title>
+      <meta name="description" content="Pokemon demo app" />
+      <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+      />
+      <link
+          rel="icon"
+          href="/assets/icons/icon-48x48.png"
+          type="image/png"
+      />
+    </Head>
+    <Component {...pageProps} />
+  </>)
 }
