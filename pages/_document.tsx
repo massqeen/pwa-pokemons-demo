536fd00cbb3abedb287ed263854f8892fdfdad33
Arthur Pliushchevskyi <ap@hilfy.net>
2023-12-06 17:37:10 +0100

    Fixed formatting

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,15 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-          <link rel='manifest' href='/manifest.webmanifest' />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html lang="en">
+            <Head>
+                <link rel='manifest' href='/manifest.webmanifest' />
+            </Head>
+            <body> 
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,6 @@ export default function Home() {
     return (
         <main>
             <h1>POKEMONS</h1>
-        </main> 
-    );
+        </main>
+    )
 }
