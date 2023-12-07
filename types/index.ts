95c961ef14db404cdb94d827d04e70f420e96746
Arthur Pliushchevskyi <ap@hilfy.net>
2023-12-07 17:29:11 +0100

    Added pokemon details type

--- types/index.ts
+++ types/index.ts
@@ -12,3 +12,31 @@ export interface IPokemon {
 export interface IPokemonsData extends IPokemonsMeta {
     results: IPokemon[]
 }
+
+export interface IBasicSprite {
+    front_default: string | null
+}
+
+export interface ISprite extends IBasicSprite {
+    back_default: string | null
+    back_female: string | null
+    back_shiny: string | null
+    back_shiny_female: string | null
+    front_female: string | null
+    front_shiny: string | null
+    front_shiny_female: string | null
+    other: {
+        dream_world: IBasicSprite
+        home: IBasicSprite
+        'official-artwork': IBasicSprite
+    }
+}
+
+export interface IPokemonDetails {
+    id:number
+    name:string
+    base_experience: number
+    height: number
+    weight: number
+    sprites: ISprite
+}
