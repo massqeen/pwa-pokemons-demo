d1bd1cfb57efc353326877105005505825e0cd3a
Arthur Pliushchevskyi <ap@hilfy.net>
2023-12-08 14:51:44 +0100

    Updated styles of the SearchInput.tsx

--- components/base/SearchInput.tsx
+++ components/base/SearchInput.tsx
@@ -16,7 +16,7 @@ const SearchInput = ({ debounceTimeout,onSearch }:IProps) => {
     }
 
     const debouncedOnSearch = useDebouncedCallback(
-        (value:string) => {
+        (value: string) => {
             onSearch(value)
         },
         debounceTimeout, { leading: true }
@@ -24,9 +24,9 @@ const SearchInput = ({ debounceTimeout,onSearch }:IProps) => {
 
     return (
         <div
-            className="max-w-md px-4 mx-auto mb-2">
+            className="max-w-xs my-4">
             <div className="relative">
-                <svg xmlns="http://www.w3.org/2000/svg" className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 left-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <svg xmlns="http://www.w3.org/2000/svg" className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 left-3" fill="none" viewBox="0 0 24 24" stroke="currentColor" style={{ width: '24px', height: '24px' }}>
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                 </svg>
                 <input
