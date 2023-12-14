f6bed0768ef529bd485137a1a4c2707adb6d103b
Arthur Pliushchevskyi <ap@hilfy.net>
2023-12-14 17:37:20 +0100

    Added sleep utility function to simulate response delay in dev mode

--- services/utils.ts
+++ services/utils.ts
@@ -2,4 +2,9 @@ function capitalizeFirstLetter(str: string): string {
     return str.charAt(0).toUpperCase() + str.slice(1)
 }
 
-export { capitalizeFirstLetter, }
+function sleep(ms = 2000): Promise<void> {
+    console.log('Kindly remember to remove `sleep`')
+    return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+export { capitalizeFirstLetter, sleep }
